7863a921a2b2a4ae9d49044f9c426191f733e848
Freddie Carthy <fcarthy@gmail.com>
2017-03-03 12:33:45 -0800

    add a initial testing

--- test/test.js
+++ test/test.js
@@ -1,9 +1,50 @@
-var assert = require('assert');
+import assert from 'assert'
+import { expect } from 'chai'
+import nock from 'nock'
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
-    });
-  });
-});
+import { sampleFetch, samplePost } from '../client/api'
+
+describe('Request', () => {
+
+  it('should fetch a user', (done) => {
+    let sampleUser = {
+        id: '1',
+        name: 'Freddie Carthy',
+        title: 'Engineer'
+    }
+
+    nock('http://localhost:8000/api')
+      .get('/exampleget')
+      .query({id: 1})
+      .reply(200, sampleUser)
+
+    sampleFetch(1).then(response => {
+      expect(response.id).to.equal(sampleUser.id)
+      expect(response.name).to.equal(sampleUser.name)
+      expect(response.title).to.equal(sampleUser.title)
+      done()
+    })
+  })
+
+  it('should save a user', (done) => {
+    let sampleUser = {
+        name: 'Taco Salad',
+        title: 'Chef'
+    }
+
+    nock('http://localhost:8000/api')
+      .post('/examplepost')
+      .reply(201, {
+        id: '2',
+        name: 'Taco Salad',
+        title: 'Chef'
+      })
+
+    samplePost(sampleUser).then(response => {
+      expect(response.id).to.equal('2')
+      expect(response.name).to.equal(sampleUser.name)
+      expect(response.title).to.equal(sampleUser.title)
+      done()
+    })
+  })
+})
