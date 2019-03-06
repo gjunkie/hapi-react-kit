a3588ac3674aad7e74e9b569e19f9089018b8907
Freddie Carthy <fcarthy@gmail.com>
2019-03-05 22:04:30 -0800

    fix routing

--- client/app.js
+++ client/app.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import { AppContainer } from 'react-hot-loader';
+import BaseApp from './containers/app/App';
 
 import reducers from './reducers';
-import Routes from './routes/Routes';
 
 const middleware = applyMiddleware(thunk);
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -16,21 +16,25 @@ export const store = createStore(
   composeEnhancers(middleware)
 );
 
-const render = (Component) => {
+const App = () => (
+  <BrowserRouter>
+    <BaseApp />
+  </BrowserRouter>
+)
+
+const render = () => {
   ReactDOM.render(
     <Provider store={store}>
-      <AppContainer>
-        <Component />
-      </AppContainer>
+      <App />,
      </Provider>,
     document.getElementById('root')
   );
 };
 
-render(Routes);
+render();
 
 if (module.hot) {
   module.hot.accept('./routes/Routes', () => {
-    render(Routes)
+    render()
   });
 }
--- client/containers/app/App.js
+++ client/containers/app/App.js
@@ -1,23 +1,22 @@
 import React, { Component } from 'react'
+import { Link, Route } from 'react-router-dom'
+import ExampleContainer from '../exampleContainer';
 import './app.css';
 
-const Link = require('react-router').Link
+const BaseApp = () => {
+  return (
+    <div>
+      <header>
+        <h1>Hapi-React-Kit</h1>
+        <ul className="nav">
+          <li><Link to={`/example/`}>Example Route</Link></li>
+        </ul>
+        <h3>You're up and running!</h3>
+      </header>
+      <div>The goal behind this boilerplate project was to ensure that the api and the client were kept separate. This is so that you can easily use a different node framework or remove React if you wish to do so.</div>
+      <Route path="/example" exact component={ExampleContainer} />
+    </div>
+  );
+};
 
-export default class App extends Component {
-
-  render() {
-    return (
-      <div>
-        <header>
-          <h1>Hapi-React-Kit</h1>
-          <ul className="nav">
-            <li><Link to={`/example/`}>Example Route</Link></li>
-          </ul>
-          <h3>You're up and running!</h3>
-        </header>
-        <div>The goal behind this boilerplate project was to ensure that the api and the client were kept separate. This is so that you can easily use a different node framework or remove React if you wish to do so.</div>
-        {this.props.children}
-      </div>
-    )
-  }
-}
+export default BaseApp;
